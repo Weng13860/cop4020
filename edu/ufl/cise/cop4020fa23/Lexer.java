96213e260e1bb92c61e0858f41f8941be5fdf7ab
Sydney Aurelius <sydneyaurelius@gmail.com>
2023-09-14 21:12:45 -0400

    Added functions to tokenize the input, determine the kind of token, and next().

--- edu/ufl/cise/cop4020fa23/Lexer.java
+++ edu/ufl/cise/cop4020fa23/Lexer.java
@@ -17,10 +17,158 @@ import edu.ufl.cise.cop4020fa23.exceptions.LexicalException;
 public class Lexer implements ILexer {
 
 	String input;
+	int previous;
+	String[] tokens;
 
 
 	public Lexer(String input) {
 		this.input = input;
+		previous = -1;
+	}
+
+	/*
+	* loop through input, parsing by space
+	* assess each token as a type
+	* if it has a space, split
+	*
+	*/
+	public String[] tokenize(input){
+		if(input == null){
+			return new String[0];
+		}
+		return input.split("\\s+");
+	}
+
+	public static Kind determineKind(String token) {
+		switch(token) {
+			case ",":
+				return Kind.COMMA;
+			case ";":
+				return Kind.SEMI;
+			case "?":
+				return Kind.QUESTION;
+			case ":":
+				return Kind.COLON;
+			case "(":
+				return Kind.LPAREN;
+			case ")":
+				return Kind.RPAREN;
+			case "<":
+				return Kind.LT;
+			case ">":
+				return Kind.GT;
+			case "[":
+				return Kind.LSQUARE;
+			case "]":
+				return Kind.RSQUARE;
+			case "=":
+				return Kind.ASSIGN;
+			case "==":
+				return Kind.EQ;
+			case "<=":
+				return Kind.LE;
+			case ">=":
+				return Kind.GE;
+			case "!":
+				return Kind.BANG;
+			case "&":
+				return Kind.BITAND;
+			case "&&":
+				return Kind.AND;
+			case "|":
+				return Kind.BITOR;
+			case "||":
+				return Kind.OR;
+			case "+":
+				return Kind.PLUS;
+			case "-":
+				return Kind.MINUS;
+			case "*":
+				return Kind.TIMES;
+			case "**":
+				return Kind.EXP;
+			case "/":
+				return Kind.DIV;
+			case "%":
+				return Kind.MOD;
+			case "<:":
+				return Kind.BLOCK_OPEN;
+			case ":>":
+				return Kind.BLOCK_CLOSE;
+			case "^":
+				return Kind.RETURN;
+			case "->":
+				return Kind.RARROW;
+			case "[]":
+				return Kind.BOX;
+			default:
+				if (token.matches("^[a-zA-Z_][a-zA-Z0-9_]*$")) {
+					// Match against all reserved words
+					switch(token) {
+						case "image":
+							return Kind.RES_image;
+						case "pixel":
+							return Kind.RES_pixel;
+						case "int":
+							return Kind.RES_int;
+						case "string":
+							return Kind.RES_string;
+						case "void":
+							return Kind.RES_void;
+						case "boolean":
+							return Kind.RES_boolean;
+						case "nil":
+							return Kind.RES_nil;
+						case "write":
+							return Kind.RES_write;
+						case "height":
+							return Kind.RES_height;
+						case "width":
+							return Kind.RES_width;
+						case "if":
+							return Kind.RES_if;
+						case "fi":
+							return Kind.RES_fi;
+						case "do":
+							return Kind.RES_do;
+						case "od":
+							return Kind.RES_od;
+						case "red":
+							return Kind.RES_red;
+						case "green":
+							return Kind.RES_green;
+						case "blue":
+							return Kind.RES_blue;
+						case "TRUE":
+							return Kind.BOOLEAN_LIT;
+						case "FALSE":
+							return Kind.BOOLEAN_LIT;
+						// Add cases for other colors like BLACK, BLUE, etc. under CONST if they're also identifiers
+						default:
+							return Kind.IDENT;
+					}
+				} else if (token.matches("^[0-9]+$")) {
+					return Kind.NUM_LIT;
+				} else if (token.startsWith("\"") && token.endsWith("\"")) {
+					return Kind.STRING_LIT;
+				}
+				// Additional logic for determining other token kinds can be added as needed
+				break;
+		}
+		return Kind.ERROR; // Default case
+	}
+
+	public IToken next(){
+		tokens = tokenize(input);
+		previous += 1;
+		// figure out kind, and make token
+		if(previous == 0){
+			return new Token(IDENT, 0, tokens[previous].length(), tokens[previous], new SourceLocation(1,1));
+		}
+		// edit source location
+		else {
+			return new Token(determineKind(tokens[previous]), tokens[previous-1].length()+1, tokens[previous].length(), tokens[previous], new SourceLocation(1,1));
+		}
 	}
 
 	@Override
