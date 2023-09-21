b170c9db770cc8ad7b5dec964adf47785e080444
jiawei <weng13860@gmail.com>
2023-09-21 16:09:10 -0400

    restart from 1

--- edu/ufl/cise/cop4020fa23/Lexer.java
+++ edu/ufl/cise/cop4020fa23/Lexer.java
@@ -7,273 +7,79 @@
  *
  * This code may not be posted on a public web site either during or after the course.
  */
-
 package edu.ufl.cise.cop4020fa23;
 
 import static edu.ufl.cise.cop4020fa23.Kind.*;
 
-import edu.ufl.cise.cop4020fa23.Token.*;
 import edu.ufl.cise.cop4020fa23.exceptions.LexicalException;
+import jdk.jfr.Percentage;
 
 import java.util.Arrays;
-
 
 public class Lexer implements ILexer {
 
     String input;
-    int previous;
+    int line;
+    int column;
+    int pos=0;
     char[] chars;
-    int line = 1;
-    int column = 1;
     char ch;
-
-    // states for numerical equations or tokens
-    private enum STATE {
-        START,
-        IN_IDENT,
-        HAVE_ZERO,
-        HAVE_DOT,
-        IN_STRING,
-        IN_FLOAT, IN_NUM, HAVE_EQ, HAVE_MINUS, HAVE_BITAND, HAVE_PLUS
-    }
-
-    // creates an array of chars from input for easier access to tokens
+    char[]chs;
+    int previous=0;
     public Lexer(String input) {
-        chars = input.toCharArray();
-        previous = 0;
-        ch = (chars.length > 0) ? chars[previous] : '\0';
+
+        this.chars=input.toCharArray();
+
+
+
+
+
     }
 
-    /*
-     * loop through input, parsing by space
-     * assess each token as a type
-     * if it has a space, split
-     *
-     */
-
-    // ensures that number is within integer range
-    public static void validateNumLit(String numStr) throws LexicalException {
-        try {
-            Integer.parseInt(numStr);
-        } catch (NumberFormatException e) {
-            throw new LexicalException();
-        }
-    }
-    // used to go through tokens one at a time
     @Override
     public IToken next() throws LexicalException {
-        STATE state = STATE.START;
-        int pos = 0;
-
-        while (true) {
-
-            switch (state) {
-                case START -> {
-                    previous = pos;
-                    switch (ch) {
-                        case '\0' -> {
-                            return new Token(EOF, previous, 0, null, new SourceLocation(line, column));
-                        }
-                        case ' ', '\t', '\r' -> {
-                            pos++;
-                            column++;
-                        }
-                        case '\n' -> {
-                            pos++;
-                            line++;
-                            column = 1;
-                        }
-                        case '-' -> {
-                            state = STATE.HAVE_MINUS;
-                            pos++;
-                            column++;
-                            ch = chars[pos];
-                        }
-                        case '+' -> {
-                            state = STATE.HAVE_PLUS;
-                            pos++;
-                            column++;
-                            ch = chars[pos];
-                        }
-                        case '&' -> {
-                            state = STATE.HAVE_BITAND;
-                            pos++;
-                            column++;
-                        }
-                        case '\"' -> {
-                            state = STATE.IN_STRING;
-                            ch = (pos < chars.length) ? chars[pos] : '\0';
-                            pos++;
-                            column++;
-                        }
-                        case 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '_', '$', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' -> {
-                            state = STATE.IN_IDENT;
-                            pos++;
-                            column++;
-                        }
-//                        case '0' -> {
-//                            state = STATE.HAVE_ZERO;
-//                            pos++;
-//                            column++;
-//                        }
-                        case '1', '2', '3', '4', '5', '6', '7', '8', '9' -> {
-                            state = STATE.IN_NUM;
-                            pos++;
-                            column++;
-                        }
-                        case '<'->{
-                            pos++;
-                            ch = (pos < chars.length) ? chars[pos] : '\0';
-                            if(ch == '='){
-                                char[] source = Arrays.copyOfRange(chars, previous, pos+1);
-                                return new Token(LE, previous, pos - previous, source, new SourceLocation(line, column));
-                            }
-                            else{
-                                char[] source = Arrays.copyOfRange(chars, previous, pos);
-                                return new Token(LT, previous, pos - previous, source, new SourceLocation(line, column));
-                            }
-                        }
-                        case ','->{
-                            pos++;
-                            line++;
-                            column++;
-                            char[] source = Arrays.copyOfRange(chars, previous, pos);
-                            return new Token(COMMA, previous, pos-previous-1, source, new SourceLocation(line, column));
-                        }
-                        case '['->{
-                            pos++;
-                            line++;
-                            column++;
-                            char[] source = Arrays.copyOfRange(chars, previous, pos);
-                            return new Token(LSQUARE, previous, pos-previous-1, source, new SourceLocation(line, column));
-                        }
-                        case ']'->{
-                            pos++;
-                            line++;
-                            column++;
-                            char[] source = Arrays.copyOfRange(chars, previous, pos);
-                            return new Token(RSQUARE, previous, pos-previous-1, source, new SourceLocation(line, column));
-                        }
-                        case '%'->{
-                            pos++;
-                            line++;
-                            column++;
-                            char[] source = Arrays.copyOfRange(chars, previous, pos);
-                            return new Token(MOD, previous, pos-previous-1, source, new SourceLocation(line, column));
-                        }
-                        case '?'->{
-                            pos++;
-                            line++;
-                            column++;
-                            char[] source = Arrays.copyOfRange(chars, previous, pos);
-                            return new Token(QUESTION, previous, pos-previous-1, source, new SourceLocation(line, column));
-                        }
-                        case ';'->{
-                            pos++;
-                            line++;
-                            column++;
-                            char[] source = Arrays.copyOfRange(chars, previous, pos);
-                            return new Token(SEMI, previous, pos-previous-1, source, new SourceLocation(line, column));
-                        }
-                        case '!'->{
-                            pos++;
-                            line++;
-                            column++;
-                            char[] source = Arrays.copyOfRange(chars, previous, pos);
-                            return new Token(BANG, previous, pos-previous-1, source, new SourceLocation(line, column));
-                        }
-                    }
-                }
-                case IN_IDENT -> {
-                    char[] source = Arrays.copyOfRange(chars, previous, pos);
-                    return new Token(IDENT, previous, pos - previous, source, new SourceLocation(line, column));
-                }
-                case IN_STRING -> {
-                    while (ch != '\"' && pos < chars.length) {
-                        pos++;
-                        ch = (pos < chars.length) ? chars[pos] : '\0';
-                    }
-                    if (ch == '\"') {
-                        pos++;  // This line moves past the closing "
-                        char[] source = Arrays.copyOfRange(chars, previous, pos);
-                        ch = (pos < chars.length) ? chars[pos] : '\0';  // Keep this line to set the next character, but don't skip any characters
-                        return new Token(STRING_LIT, previous, pos - previous, source, new SourceLocation(line, column));
-                    } else {
-                        throw new LexicalException("Unterminated string literal.");
-                    }
-                }
-                // if the number is 0
-//                case HAVE_ZERO -> {
-//                    char[] source = Arrays.copyOfRange(chars, previous, pos);
-//                    return new Token(NUM_LIT, previous, pos-previous, source, new SourceLocation(line, column));
-//
-//                }
+        pos=previous;
 
 
-                // if the number is a decimal
-                case HAVE_DOT -> {
-//                    char[] source = Arrays.copyOfRange(chars, previous, pos);
-//                    return new Token(NUM_LIT, previous, pos - previous, source, new SourceLocation(line, column));
-                }
-
-                // for multiple digit numbers
-                case IN_NUM -> {
-                    while (Character.isDigit(ch) && pos < chars.length) {
-                        if(ch == '0'){
-                            break;
-                        }
-                        pos++;
-                        if (pos < chars.length) {
-                            ch = chars[pos];
-                        }
-                    }
-                    String numStr = new String(chars, previous, pos - previous);
-                    validateNumLit(numStr);
-                    char[] source = Arrays.copyOfRange(chars, previous, pos);
-                    return new Token(NUM_LIT, previous, pos-previous, source, new SourceLocation(line, column));
-                }
-
-                // equation
-                case HAVE_MINUS -> {
-                    if (ch == '>') {
-                        pos++;
-                        column++;
-                        char[] source = Arrays.copyOfRange(chars, previous, pos);
-                        return new Token(RARROW, previous, 2, source, new SourceLocation(line, column));
-                    }
-                }
-                case HAVE_PLUS -> {
-                    pos++;
-                    column++;
-                    char[] source = Arrays.copyOfRange(chars, previous, pos);
-                    return new Token(PLUS, previous, 2, source, new SourceLocation(line, column));
-                }
-                case HAVE_BITAND -> {
-                    if (ch == '&') {
-                        pos++;
-                        char[] source = Arrays.copyOfRange(chars, previous, pos);
-                        ch = (pos < chars.length) ? chars[pos] : '\0';
-                        return new Token(AND, previous, 2, source, new SourceLocation(line, column));
-                    }
-                    else {
-                        char[] source = Arrays.copyOfRange(chars, previous, pos);
-                        return new Token(BITAND, previous, 1, source, new SourceLocation(line, column));
-                    }
-                }
-                case IN_FLOAT -> {
-
-                }
-
+        while(true){
+            ch = (pos < chars.length) ? chars[pos] : '\0';
+            if(ch == '\0'){
+                return new Token(EOF, 0, 0, null, new SourceLocation(1, 1));
             }
+
             pos++;
-            if (pos < chars.length) {
-                ch = chars[pos];
-            } else {
-                ch = '\0';
+
+            switch(ch){
+            case ','->{
+                char[]source=Arrays.copyOfRange(chars,previous,pos);
+                previous=pos;
+                return new Token(COMMA,pos,1,source,new SourceLocation(line,column));
+
             }
+            case '['->{
+                char[]source=Arrays.copyOfRange(chars,previous,pos);
+                previous=pos;
+                return new Token(LSQUARE,pos,1,source,new SourceLocation(line,column));
+
+            }
+            case']'->{
+                char[]source=Arrays.copyOfRange(chars,previous,pos);
+                previous=pos;
+                return new Token(RSQUARE,pos,1,source,new SourceLocation(line,column));
+            }
+            case '%'->{
+                char[]source=Arrays.copyOfRange(chars,previous,pos);
+                previous=pos;
+                return new Token(MOD,pos,1,source,new SourceLocation(line,column));
+            }
+            case '+'->{
+                char[]source=Arrays.copyOfRange(chars,previous,pos);
+                previous=pos;
+                return new Token(PLUS,pos,1,source,new SourceLocation(line,column));
+            }
+            }
+
         }
 
-    }
 
-
-}
+    }}
