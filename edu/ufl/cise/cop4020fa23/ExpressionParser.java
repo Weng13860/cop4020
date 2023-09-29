5378e90ceeb00d8b18be5e89e9bd43c96de1f0af
jiawei <weng13860@gmail.com>
2023-09-29 16:54:02 -0400

    re edit for whole assignment

--- edu/ufl/cise/cop4020fa23/ExpressionParser.java
+++ edu/ufl/cise/cop4020fa23/ExpressionParser.java
@@ -58,21 +58,206 @@ public class ExpressionParser implements IParser {
 
 
 	@Override
-	public AST parse() throws PLCCompilerException {
+	public Expr parse() throws PLCCompilerException {
 		Expr e = expr();
 		return e;
+	}
+
+	private void consume() throws PLCCompilerException {
+		try {
+			t = lexer.next();
+		} catch (Exception e) {
+			System.out.println("nonononono");
+		}
 	}
 
 
 	private Expr expr() throws PLCCompilerException {
 		IToken firstToken = t;
-		if(isPrimaryExprToken(firstToken)){
-			return PrimaryExpr();
+		if(firstToken.kind()==Kind.QUESTION){return ConditionalExpr();}
+
+		else {return LogicalOrExpr();}}
+	private Expr ConditionalExpr() throws  PLCCompilerException{
+		IToken firstToken = t;
+		Expr x=null;
+		Expr y=null;
+		Expr z=null;
+		if(t.kind()!=Kind.QUESTION){
+			throw new SyntaxException("111");
+		}else{
+			consume();
+			x=expr();
+			if(t.kind()!=Kind.RARROW){
+				throw new SyntaxException("aaa");
+			}
+			else {consume();
+				y=expr();
+				if(t.kind()!=Kind.COMMA){
+					throw new SyntaxException("bbb");}
+					else{
+						consume();
+						z=expr();
+					}
+				}
+			}
+
+
+		return new ConditionalExpr(firstToken,x,y,z);
+	}
+	private Expr LogicalOrExpr()throws PLCCompilerException{
+		IToken firstToken = t;
+		Expr x=null;
+		Expr y=null;
+		x=LogicalAndExpr();
+		while(t.kind()==Kind.BITOR||t.kind()==Kind.OR){
+			IToken op=t;
+			consume();
+			y=LogicalAndExpr();
+			x=new BinaryExpr(firstToken,x,op,y);
 		}
+		return x;
+
+	}
+	private Expr LogicalAndExpr()throws PLCCompilerException{
+		IToken firstToken = t;
+		Expr x=null;
+		Expr y=null;
+		x=ComparsionExpr();
+		while(t.kind()==Kind.BITAND||t.kind()==Kind.AND){
+			IToken op=t;
+			consume();
+			y= ComparsionExpr();
+			x=new BinaryExpr(firstToken,x,op,y);
+		}
+		return x;
+
+	}
+	private Expr ComparsionExpr() throws PLCCompilerException{
+		IToken firstToken = t;
+		Expr x=null;
+		Expr y=null;
+		x=PowExpr();
+		while(t.kind()==Kind.GT||t.kind()==Kind.GE||t.kind()==Kind.LE||t.kind()==Kind.LT||t.kind()==Kind.EQ){
+			IToken firstleft=t;
+			consume();
+			y= PowExpr();
+			x=new BinaryExpr(firstToken,x,firstleft,y);
+		}
+		return x;
+
+	}
+	private Expr PowExpr()throws PLCCompilerException{
+		IToken firstToken = t;
+		Expr x=null;
+		Expr y=null;
+		x=AdditiveExpr();
+		if(x!=null){
+			consume();
+			if(t.kind()==Kind.EXP){
+				consume();
+				y=PowExpr();
+				return PowExpr();
+			}
+			else if(t.kind()!=null){
+				throw new SyntaxException("powexpr wrong1");
+			}
+			else{return AdditiveExpr();}
+		}
+		else throw new SyntaxException("powexpr wrong2");
+	}
+	private Expr AdditiveExpr()throws PLCCompilerException{
+		IToken firstToken = t;
+		Expr x=null;
+		Expr y=null;
+		x=MultiplicativeExpr();
+		while(t.kind()==Kind.PLUS||t.kind()==Kind.MINUS){
+			IToken op=t;
+			consume();
+			y=MultiplicativeExpr();
+			x=AdditiveExpr();
+
+		}
+		return x;
+	}
+	private Expr MultiplicativeExpr()throws PLCCompilerException{
+
+		IToken firstToken = t;
+		Expr x=null;
+		Expr y=null;
+		x=UnaryExpr();
+		while(t.kind()==Kind.TIMES||t.kind()==Kind.DIV||t.kind()==Kind.MOD){
+			IToken op=t;
+			consume();
+			y=UnaryExpr();
+			x=MultiplicativeExpr();
+
+		}
+		return x;
+	}
+	private Expr UnaryExpr()throws PLCCompilerException{
+		IToken firstToken = t;
+		Expr x=null;
+		Expr y=null;
+		if(t.kind()!=Kind.BANG||t.kind()!=Kind.MINUS||t.kind()!=Kind.RES_width||t.kind()!=Kind.RES_height){throw new SyntaxException("UnaryExpr p");}
+		else {consume();
+			x=UnaryExpr();
+			if(x!=null){
+				return x;}
 			else{
-				throw new UnsupportedOperationException("THE PARSER HAS NOT BEEN IMPLEMENTED YET");
+				x=PostfixExpr();
+				if(x!=null){return x;}
+				else {
+				throw new SyntaxException("UnartEXpr2");}
+
 			}
 		}
+	}
+	private Expr PostfixExpr()throws PLCCompilerException{
+		if
+	}
+	private Expr ChannelSelector()throws PLCCompilerException{
+		IToken firstToken = t;
+		IToken color;
+		Expr x=null;
+		if(t.kind()!=Kind.COLON){throw new SyntaxException("pro ChannelS");}
+		else{
+			consume();
+			if(t.kind()==Kind.RES_green||t.kind()==Kind.RES_red||t.kind()==Kind.RES_blue){
+				color=IToken.Kind();
+
+
+				return ChannelSelector(firstToken,);
+			}
+		}
+	}
+
+
+	private Expr ExpandedPixelExpr()throws PLCCompilerException{
+		IToken firstToken=t;
+		Expr x=null;
+		Expr y=null;
+		Expr z=null;
+		if(firstToken.kind()==Kind.LSQUARE){
+			consume();
+			x=expr();
+			if(firstToken.kind()==Kind.COMMA){
+				consume();
+				y=expr();
+				if(firstToken.kind()==Kind.RSQUARE){
+					consume();
+					return new ExpandedPixelExpr(firstToken,x,y,z);
+				}
+				else return null;
+			}
+			else return null;
+		}
+		else return null;
+	}
+
+
+
+
+
 
 	public Expr PrimaryExpr() throws PLCCompilerException{
 		IToken firstToken = t;
