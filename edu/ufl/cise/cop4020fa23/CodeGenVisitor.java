01fdd185ba03e580c45aedeea8b80c7bf00517a6
Sydney Aurelius <sydneyaurelius@gmail.com>
2023-11-11 14:22:37 -0500

    edited CodeGenVisitor to return name of visit

---
+++ edu/ufl/cise/cop4020fa23/CodeGenVisitor.java
@@ -0,0 +1,160 @@
+package edu.ufl.cise.cop4020fa23;
+
+import edu.ufl.cise.cop4020fa23.ast.*;
+import edu.ufl.cise.cop4020fa23.exceptions.PLCCompilerException;
+
+public class CodeGenVisitor implements ASTVisitor {
+
+    private StringBuilder javaCode;
+    private static String packageName;
+
+    public CodeGenVisitor(String packageName) {
+        this.packageName = packageName;
+        this.javaCode = new StringBuilder();
+    }
+
+    public static String getPackageName(){
+        return packageName;
+    }
+    @Override
+    public Object visitAssignmentStatement(AssignmentStatement assignmentStatement, Object arg) throws PLCCompilerException {
+        Object expressionResult = assignmentStatement.getE().visit(this, arg);
+
+        javaCode.append("  ").append(assignmentStatement.getlValue().getName())
+                .append(" = ").append(expressionResult)
+                .append(";\n");
+
+        return javaCode.toString();
+    }
+
+    @Override
+    public Object visitBinaryExpr(BinaryExpr binaryExpr, Object arg) throws PLCCompilerException {
+        String left = binaryExpr.getLeftExpr().visit(this, arg).toString();
+        String operator = binaryExpr.getOpKind().toString();
+        String right = binaryExpr.getRightExpr().visit(this, arg).toString();
+        return "(" + left + " " + operator + " " + right + ")";
+    }
+
+    @Override
+    public Object visitBlock(Block block, Object arg) throws PLCCompilerException {
+        for (Block.BlockElem elem : block.getElems()) {
+            elem.visit(this, arg);
+        }
+        return javaCode.toString();
+    }
+
+    @Override
+    public Object visitBlockStatement(StatementBlock statementBlock, Object arg) throws PLCCompilerException {
+        return "Block Statement";
+    }
+
+    @Override
+    public Object visitBooleanLitExpr(BooleanLitExpr booleanLitExpr, Object arg) throws PLCCompilerException {
+        return booleanLitExpr.getText();
+    }
+
+    @Override
+    public Object visitChannelSelector(ChannelSelector channelSelector, Object arg) throws PLCCompilerException {
+        return "Channel Selector";
+    }
+
+    @Override
+    public Object visitConditionalExpr(ConditionalExpr conditionalExpr, Object arg) throws PLCCompilerException {
+        return "Conditional Expr";
+    }
+
+    @Override
+    public Object visitConstExpr(ConstExpr constExpr, Object arg) throws PLCCompilerException {
+        return "Const Expr";
+    }
+
+    @Override
+    public Object visitDeclaration(Declaration declaration, Object arg) throws PLCCompilerException {
+        return "Declaration";
+    }
+
+    @Override
+    public Object visitDimension(Dimension dimension, Object arg) throws PLCCompilerException {
+        return "Dimension";
+    }
+
+    @Override
+    public Object visitDoStatement(DoStatement doStatement, Object arg) throws PLCCompilerException {
+        return "Do";
+    }
+
+    @Override
+    public Object visitExpandedPixelExpr(ExpandedPixelExpr expandedPixelExpr, Object arg) throws PLCCompilerException {
+        return "Expanded Pixel";
+    }
+
+    @Override
+    public Object visitGuardedBlock(GuardedBlock guardedBlock, Object arg) throws PLCCompilerException {
+        return "Guarded Block";
+    }
+
+    @Override
+    public Object visitIdentExpr(IdentExpr identExpr, Object arg) throws PLCCompilerException {
+        return identExpr.getNameDef().getJavaName();
+    }
+
+    @Override
+    public Object visitIfStatement(IfStatement ifStatement, Object arg) throws PLCCompilerException {
+        return "If";
+    }
+
+    @Override
+    public Object visitLValue(LValue lValue, Object arg) throws PLCCompilerException {
+        return "L Value";
+    }
+
+    @Override
+    public Object visitNameDef(NameDef nameDef, Object arg) throws PLCCompilerException {
+        return nameDef.getJavaName();
+    }
+
+    @Override
+    public Object visitNumLitExpr(NumLitExpr numLitExpr, Object arg) throws PLCCompilerException {
+        return numLitExpr.getText();
+    }
+
+    @Override
+    public Object visitPixelSelector(PixelSelector pixelSelector, Object arg) throws PLCCompilerException {
+        return "Pixel Selector";
+    }
+
+    @Override
+    public Object visitPostfixExpr(PostfixExpr postfixExpr, Object arg) throws PLCCompilerException {
+        return "Postfix";
+    }
+
+    @Override
+    public Object visitProgram(Program program, Object arg) throws PLCCompilerException {
+        return "Program";
+    }
+
+    @Override
+    public Object visitReturnStatement(ReturnStatement returnStatement, Object arg) throws PLCCompilerException {
+        return "Return";
+    }
+
+    @Override
+    public Object visitStringLitExpr(StringLitExpr stringLitExpr, Object arg) throws PLCCompilerException {
+        return "\"" + stringLitExpr.getText() + "\"";
+    }
+
+    @Override
+    public Object visitUnaryExpr(UnaryExpr unaryExpr, Object arg) throws PLCCompilerException {
+        return "Unary";
+    }
+
+    @Override
+    public Object visitWriteStatement(WriteStatement writeStatement, Object arg) throws PLCCompilerException {
+        return "Write";
+    }
+
+    public String getGeneratedCode() {
+        return javaCode.toString();
+    }
+
+}
